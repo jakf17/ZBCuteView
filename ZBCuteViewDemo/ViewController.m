b69e31bf79b47e34c1d520c518a7ef11878f3f01
yangzhibo <yangzhibo@pyyx.com>
2017-06-23 12:43:03 +0800

    UIBezierPath work in with CADisplayLink

--- ZBCuteViewDemo/ViewController.m
+++ ZBCuteViewDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ZBCuteView.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    ZBCuteView *cuteView = [[ZBCuteView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+    cuteView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:cuteView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
---
+++ ZBCuteViewDemo/ZBCuteView/ZBCuteView.h
@@ -0,0 +1,14 @@
+//
+//  ZBCuteView.h
+//  ZBCuteViewDemo
+//
+//  Created by jakf_17 on 17/6/23.
+//  Copyright © 2017年 jakf_17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZBCuteView : UIView
+
+@end
+
